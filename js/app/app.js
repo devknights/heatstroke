6c4b1f4f0ed3cab886149ef22435c7ec01762a2f
munkychop <ivan@Ivans-MacBook-Air.local>
2014-03-19 22:16:12 +0000

    added basic structure to character class.

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,9 @@
-var _character = new Character ();
-var _level = new Level ();
+
+var _character1 = new Character ();
+_character1.init (document.getElementById("hero"));
+
 var _keyboardInteraction = new KeyboardInteraction ();
+
+
+
+//var _level = new Level ();
--- js/app/character.js
+++ js/app/character.js
@@ -2,7 +2,16 @@ function Character ()
 {
 	var _self = this;
 
-	_self.init = function ()
+	_self.el;
+
+	_self.init = function (characterEl)
 	{
-	}
+		_self.el = characterEl;
+	};
+
+	_self.move = function (targetPoint)
+	{
+		_self.el.style.left = targetPoint.x + "px";
+		_self.el.style.top = targetPoint.y + "px";
+	};
 }
